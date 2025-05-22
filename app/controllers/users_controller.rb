e0e76ee94487434bb336280003261576843caa03
shun-radiation <mizokami.shunsuke.it@gmail.com>
2025-05-23 00:41:11 +0900

    user sign-up failed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
     @user = User.new
     # debugger
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
