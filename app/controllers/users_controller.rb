01cfdd7b31e3136ac5a777fc33841a7e64fcfea0
shun-radiation <mizokami.shunsuke.it@gmail.com>
2025-05-24 21:19:13 +0900

    sign-up success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App"
+      redirect_to @user  # redirect_to user_url(@user)
+
     else
       render "new", status: :unprocessable_entity
     end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,11 +13,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                         password: "foo",
                                         password_confirmation: "bar" } }
     end
+
     assert_response :unprocessable_entity
     assert_template "users/new"
 
     assert_select "div#error_explanation"
     assert_select "div.alert"
     assert_select "div.alert-danger"
+    # assert_not flash.empty?
+  end
+
+  test "valid signup information" do
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: { name: "Example User",
+                                      email: "user@example.com",
+                                      password: "password",
+                                      password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.empty?
   end
 end
