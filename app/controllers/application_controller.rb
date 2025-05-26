fdc20068ea8636a75800d39598e9621552c48c62
shun-radiation <mizokami.shunsuke.it@gmail.com>
2025-05-26 17:08:08 +0900

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+
+  # allow_browser versions: :modern
+  if Rails.env.production?
+    allow_browser versions: :modern
+  end
+
+
+  include SessionsHelper
 end
---
+++ app/javascript/custom/menu.js
@@ -0,0 +1,13 @@
+// メニュー操作
+
+// トグルリスナーを追加してクリックをリッスンする
+// document.addEventListener('turbo:load', () => {
+//   let account = document.querySelector('#account');
+//   if (account) {
+//     account.addEventListener('click', (event) => {
+//       event.preventDefault();
+//       let menu = document.querySelector('#dropdown-menu');
+//       menu.classList.toggle('active');
+//     });
+//   }
+// });
