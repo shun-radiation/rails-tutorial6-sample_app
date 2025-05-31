3a0e44367fe77ee2736b005d2e0a93f37b4d98df
shun-radiation <mizokami.shunsuke.it@gmail.com>
2025-05-31 18:19:35 +0900

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -76,4 +76,20 @@ class LogoutTest < Logout
     delete logout_path
     assert_redirected_to root_url
   end
+
+  class RememberingTest < UsersLogin
+    test "login with remembering" do
+      log_in_as(@user, remember_me: "1")
+      # assert_not cookies[:remember_token].blank?
+      assert_equal cookies[:remember_token], assigns(:user).remember_token
+    end
+
+    test "login without remembering" do
+      # Cookieを保存してログイン
+      log_in_as(@user, remember_me: "1")
+      # Cookieが削除されていることを検証してからログイン
+      log_in_as(@user, remember_me: "0")
+      assert cookies[:remember_token].blank?
+    end
+  end
 end
