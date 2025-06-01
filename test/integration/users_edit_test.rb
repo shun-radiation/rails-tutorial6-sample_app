fbe8d4aac0d864e5790f3d048997be77e15eca20
shun-radiation <mizokami.shunsuke.it@gmail.com>
2025-06-02 04:06:50 +0900

    Authorization

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,11 +5,14 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  require "test_helper"
+
   def setup
     @user = users(:michael)
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template "users/edit"
     patch user_path(@user), params: { user: { name: "",
@@ -20,9 +23,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select "div.alert", text: "The form contains 4 errors."
   end
 
-  test "successful edit" do
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template "users/edit"
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name: name,
@@ -34,5 +38,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+    assert_nil session[:forwarding_url]
+    delete logout_path
+    log_in_as @user
+    assert_redirected_to @user
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
